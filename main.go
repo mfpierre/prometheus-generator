3fe9a96545b73565f37826b4296662ab490c19e1
Pierre <pierre.margueritte@datadoghq.com>
2019-08-07 15:42:40 +0200

    Add histogram

--- main.go
+++ main.go
@@ -3,17 +3,20 @@ package main
 import (
 	"flag"
 	"log"
+	"math/rand"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
-	addr     = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
-	gauges   = flag.Int("gauges", 100, "Number of gauges to generate")
-	counters = flag.Int("counters", 100, "Number of counters to generate")
+	addr                     = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
+	gauges                   = flag.Int("gauges", 100, "Number of gauges to generate")
+	counters                 = flag.Int("counters", 100, "Number of counters to generate")
+	httpRequestsResponseTime prometheus.Histogram
 )
 
 func main() {
@@ -37,7 +40,26 @@ func main() {
 		prometheus.MustRegister(c)
 	}
 
+	httpRequestsResponseTime = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Namespace: "http",
+		Name:      "response_time_seconds",
+		Help:      "Request response times",
+	})
+	prometheus.MustRegister(httpRequestsResponseTime)
+
 	// Expose the registered metrics via HTTP.
-	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe(*addr, nil))
+	handler := http.NewServeMux()
+	handler.Handle("/metrics", promhttp.Handler())
+	withMetrics := middleware(handler)
+	log.Fatal(http.ListenAndServe(*addr, withMetrics))
+}
+
+func middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		// have a better distribution
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		httpRequestsResponseTime.Observe(float64(time.Since(start).Seconds()))
+	})
 }
